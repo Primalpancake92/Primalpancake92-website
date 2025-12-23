e67121fd225c56cd847da2c98ad4f613f06c0973
Primalpancake92 <johnle.jl39@gmail.com>
2025-12-23 11:56:55 +1100

    Added the "Experience" page with the experiences and skills added.

--- scripts/projects.js
+++ scripts/projects.js
@@ -15,7 +15,7 @@ const lastItem = items[totalItems - 1];
 backBtn.disabled = true;
 
 function moveItems() {
-    const offset = index * (itemWidth + 50)
+    const offset = index * (itemWidth + 35)
 
     imageSlider.style.transform = `translate(${-offset}px)`
 
@@ -26,7 +26,7 @@ function moveItems() {
 forwardBtn.addEventListener('click', () => {
     forwardBtn.style.animation = "none"
     forwardBtn.offsetHeight
-    forwardBtn.style.animation = "bounce 0.15s linear"
+    forwardBtn.style.animation = "bounce 0.1s linear"
     if (index < totalItems - 3) {
         index += 1
         moveItems()
@@ -35,10 +35,33 @@ forwardBtn.addEventListener('click', () => {
 
 backBtn.addEventListener('click', () => {
     backBtn.style.animation = "none"
-    backBtn.offsetHeight
-    backBtn.style.animation = "bounce 0.15s linear"
+    backBtn.offsetHeight;
+    backBtn.style.animation = "bounce 0.1s linear"
     if (index > 0) {
         index -= 1
         moveItems()
     }
-})
+}); 
+
+const projectLinks = document.querySelectorAll(".project-link");
+
+projectLinks.forEach(link => {
+    const seeMoreText = link.querySelector(".see-more p");
+    const arrow = link.querySelector(".see-more i");
+
+    link.addEventListener("mouseenter", () => {
+        arrow.style.transform = "rotate(45deg)";
+        seeMoreText.style.animation = "opacity-more 0.3s ease-in-out forwards";
+        seeMoreText.style.display = "flex";
+        link.style.animation = 'see-more 0.3s ease-in-out forwards';
+    });
+
+    link.addEventListener("mouseleave", () => {
+        arrow.style.transform = "rotate(0deg)";
+        setTimeout(() => {
+            link.style.animation = 'see-less 0.3s ease-in-out forwards';
+            seeMoreText.style.animation = "opacity-less 0.3s ease-in-out forwards";
+            seeMoreText.style.display = "none";
+        }, 5);
+    });
+});
--- scripts/stuff.js
+++ scripts/stuff.js
@@ -1,6 +1,7 @@
 const firstDot = document.querySelector('.dots-1');
 const secDot = document.querySelector('.dots-2');
 const thirdDot = document.querySelector('.dots-3');
+const fourthDot = document.querySelector('.dots-4');
 
 window.addEventListener("DOMContentLoaded", () => {
     firstDot.style.animation = 'bubbles 7s ease-in-out infinite';
@@ -12,6 +13,10 @@ window.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
         thirdDot.style.animation = 'bubbles 7s ease-in-out infinite';
     }, 2000);
+
+    setTimeout(() => {
+        fourthDot.style.animation = 'bubbles 7s ease-in-out infinite';
+    }, 5000);
 });
 
 const toHomeLink = document.querySelector('.to-home a');
@@ -23,3 +28,13 @@ toHomeLink.addEventListener('mouseover', () => {
 toHomeLink.addEventListener('mouseout', () => {
     toHomeLink.style.animation = 'none'; 
 })
+
+const arrayOfSocials = document.querySelectorAll(".social-bub");
+console.log(Array.from(arrayOfSocials));
+
+for (let i = 0; i < Array.from(arrayOfSocials).length ; i++) {
+    setTimeout(() => {
+        arrayOfSocials[i].style.animation = "social-bounce 2s ease-in-out infinite";
+    }, i * 400);
+    
+};
